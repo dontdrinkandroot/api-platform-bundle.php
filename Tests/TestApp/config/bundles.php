4c670d06e42dec65256a48d1f2dbcfefc27589d4
Philip Washington Sorst <philip@sorst.net>
2020-07-20 22:59:59 +0200

    CI: Adding travis, basic test setup

---
+++ Tests/TestApp/config/bundles.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class           => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class                     => ['all' => true],
+    ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
+    Dontdrinkandroot\ApiPlatformBundle\DdrApiPlatformBundle::class  => ['all' => true],
+];
