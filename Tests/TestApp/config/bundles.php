04a9f7473dec05a4f5e0b9b75a6397880a9e9e75
Philip Washington Sorst <philip@sorst.net>
2020-07-27 20:35:08 +0200

    Tests

--- Tests/TestApp/config/bundles.php
+++ Tests/TestApp/config/bundles.php
@@ -2,6 +2,8 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class           => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class            => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class             => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class                     => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Dontdrinkandroot\ApiPlatformBundle\DdrApiPlatformBundle::class  => ['all' => true],
