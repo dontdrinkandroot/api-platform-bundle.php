4d7c8f6bddc30f7de216324fdb7e4899731fb75e
Philip Washington Sorst <philip@sorst.net>
2023-01-22 20:35:19 +0100

    tests: Adding OutputRoutesTest

---
+++ tests/Acceptance/OutputRoutesTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Dontdrinkandroot\ApiPlatformBundle\Tests\Acceptance;
+
+use Dontdrinkandroot\Common\Asserted;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\Routing\RouterInterface;
+
+class OutputRoutesTest extends WebTestCase
+{
+    public function testRoutes(): void
+    {
+        $this->markTestSkipped('Useful test for debugging');
+
+        $router = Asserted::instanceOf(self::getContainer()->get(RouterInterface::class), RouterInterface::class);
+        self::assertNotEmpty($router->getRouteCollection());
+
+        foreach ($router->getRouteCollection()->all() as $name => $route) {
+            echo sprintf(
+                "[name: %s, methods: %s, path: %s, controller: %s]%s",
+                $name,
+                implode(',', $route->getMethods()),
+                $route->getPath(),
+                $route->getDefault('_controller'),
+                PHP_EOL
+            );
+        }
+    }
+}
