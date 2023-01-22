38bbfff78ef744af6c3c0295f554fee728c1f5af
Philip Washington Sorst <philip@sorst.net>
2023-01-22 20:35:51 +0100

    tests: Cleanup

--- tests/TestApp/config/bundles.php
+++ tests/TestApp/config/bundles.php
@@ -4,7 +4,6 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class           => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class            => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class             => ['all' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class                     => ['all' => true],
     ApiPlatform\Core\Bridge\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class    => ['test' => true],
     Liip\TestFixturesBundle\LiipTestFixturesBundle::class           => ['test' => true],
