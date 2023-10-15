a1b2de8110d01562fa0f8085819c917e554e4550
Philip Washington Sorst <philip@sorst.net>
2023-10-15 23:23:25 +0200

    refactor: Fixing psalm issues

--- tests/Acceptance/OutputRoutesTest.php
+++ tests/Acceptance/OutputRoutesTest.php
@@ -10,7 +10,7 @@ class OutputRoutesTest extends WebTestCase
 {
     public function testRoutes(): void
     {
-//        $this->markTestSkipped('Useful test for debugging');
+        $this->markTestSkipped('Useful test for debugging');
 
         $router = Asserted::instanceOf(self::getContainer()->get(RouterInterface::class), RouterInterface::class);
         self::assertNotEmpty($router->getRouteCollection());
